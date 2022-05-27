6414a454b8ba74ab21cf6ed93effae452e5fc6b7
Ivan <imedrano_@ucol.mx>
2022-05-27 16:00:13 -0500

    Se agregaron las fichas bibliograficas y modifico el estilo principal del trigger

--- src/components/BibliographicCards.jsx
+++ src/components/BibliographicCards.jsx
@@ -7,12 +7,12 @@ const BibliographicCards = () => {
   const [activeArticle, setActiveArticle] = useState({
     // ! Como estado por defecto deja la primera ficha
     id: 1,
-    title: "Título",
+    title: "El arte de la Guerra",
     description:
-      "Nostrud anim commodo eiusmod proident cupidatat irure commodo enim pariatur. Esse laboris proident est etVelit sunt sint exercitation in aliquip veniam cillum. Quis deserunt ut laboris nostrud tempor sint.",
-    link: "https://google.com",
+      "Bibliografía. México: Anaya Editores, 2007. El Arte de la guerra ilustrado / Sun Tzu ; versión de Thomas Cleary. Madrid: EDAF, 2004.",
+    link: "https://www.amazon.com.mx/El-Arte-Guerra-Cl%C3%A1sicos-literatura-ebook/dp/B011VIVOFO",
     image:
-      "https://www.xtrafondos.com/wallpapers/vertical/star-lord-marvels-guardianes-de-la-galaxia-8283.jpg",
+      "https://i.imgur.com/zLhcylu.png",
   });
 
   const setArticle = (value) => {
@@ -20,60 +20,60 @@ const BibliographicCards = () => {
       case 1:
         setActiveArticle({
           id: 1,
-          title: "Título uno",
+          title: "El arte de la guerra",
           description:
-            "Nostrud anim commodo eiusmod proident cupidatat irure commodo enim pariatur. Esse laboris proident est etVelit sunt sint exercitation in aliquip veniam cillum. Quis deserunt ut laboris nostrud tempor sint.",
-          link: "https://google.com",
+            "Bibliografía. México: Anaya Editores, 2007. El Arte de la guerra ilustrado / Sun Tzu ; versión de Thomas Cleary. Madrid: EDAF, 2004.",
+          link: "https://www.amazon.com.mx/El-Arte-Guerra-Cl%C3%A1sicos-literatura-ebook/dp/B011VIVOFO",
           image:
-            "https://www.xtrafondos.com/wallpapers/vertical/star-lord-marvels-guardianes-de-la-galaxia-8283.jpg",
+            "https://i.imgur.com/zLhcylu.png",
         });
         return;
 
       case 2:
         setActiveArticle({
           id: 2,
-          title: "Título dos",
+          title: "Dracula: Deluxe Edition",
           description:
-            "Nostrud anim commodo eiusmod proident cupidatat irure commodo enim pariatur.",
-          link: "https://google.com",
+            "Stoker, B., & Gorey, E. (2021). Dracula: Deluxe Edition. Union Square & Co.",
+          link: "https://www.amazon.com.mx/Dracula-Deluxe-Bram-Stoker/dp/1454944218",
           image:
-            "https://images.unsplash.com/photo-1515260268569-9271009adfdb?w=240&h=320&fit=crop",
+            "https://i.imgur.com/LeGS4W6.png",
         });
         return;
 
       case 3:
         setActiveArticle({
           id: 3,
-          title: "Título tres",
+          title: "Dime quien soy",
           description:
-            "Nostrud anim commodo eiusmod proident cupidatat irure commodo enim pariatur.",
-          link: "https://google.com",
+            "Navarro, J. (2013). Dime quién soy (1.a ed.). Penguin Random House Grupo Editorial SA de CV.",
+          link: "https://www.amazon.com.mx/Dime-qui%C3%A9n-soy-Julia-Navarro/dp/6073105142/ref=asc_df_6073105142/?tag=gledskshopmx-20&linkCode=df0&hvadid=295447233299&hvpos=&hvnetw=g&hvrand=9455093088528404515&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9073783&hvtargid=pla-435427430315&psc=1",
           image:
-            "https://images.unsplash.com/photo-1506045412240-22980140a405?w=240&h=320&fit=crop",
+            "https://i.imgur.com/94ZHyP1.png",
         });
         return;
 
       case 4:
         setActiveArticle({
           id: 4,
-          title: "Título cuatro",
+          title: "El guardian entre el centeno",
           description:
-            "Nostrud anim commodo eiusmod proident cupidatat irure commodo enim pariatur.",
+            "Salinger, J. (2021). The Catcher in the Rye (Reissue ed.). Little Brown.",
           link: "https://google.com",
           image:
-            "https://images.unsplash.com/photo-1514041181368-bca62cceffcd?w=240&h=320&fit=crop",
+            "https://i.imgur.com/fWT4SXp.png",
         });
         return;
 
       case 5:
         setActiveArticle({
           id: 5,
-          title: "Título cinco!!!",
+          title: "Cementerio de animales",
           description:
-            "Nostrud anim commodo eiusmod proident cupidatat irure commodo enim pariatur.",
-          link: "https://google.com",
+            "King, S., & Suárez, D. A. L. F. M. (2012). Cementerio de animales. DEBOLS!LLO.",
+          link: "https://www.amazon.com.mx/Cementerio-Animales-Cemetary-Stephen-King/dp/8497930991",
           image:
-            "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=240&h=320&fit=crop",
+            "https://i.imgur.com/cNMn0xM.png",
         });
         return;
 
@@ -95,6 +95,7 @@ const BibliographicCards = () => {
     <Layout id="bibliograficCards">
       <h1 className="text-center section-title">Fichas bibliográficas</h1>
       <hr />
+      <h3 className="mt-5 mb-5 ms-5 d-flex activities-metaverse">Para observar estas fichas es necesario tener instalada la app Zappar</h3>
       <ArticleContainer className="bibligraphic-card mt-4">
         <ArticleImage src={activeArticle.image} alt={activeArticle.title} />
         <ArticleInfo className="bibliographic-card-info">
@@ -109,7 +110,7 @@ const BibliographicCards = () => {
         <ArticleOption
           className={activeArticle.id === 1 ? "active-card" : 0}
           type="image"
-          src="https://www.xtrafondos.com/wallpapers/vertical/star-lord-marvels-guardianes-de-la-galaxia-8283.jpg"
+          src="https://ss365.liverpool.com.mx/xl/1115314862.jpg"
           onClick={() => {
             setArticle(1);
           }}
@@ -117,7 +118,7 @@ const BibliographicCards = () => {
         <ArticleOption
           className={activeArticle.id === 2 ? "active-card" : 0}
           type="image"
-          src="https://images.unsplash.com/photo-1515260268569-9271009adfdb?w=125&h=125&fit=crop"
+          src="https://images-na.ssl-images-amazon.com/images/I/51b5eWrRawS._SX370_BO1,204,203,200_.jpg"
           onClick={() => {
             setArticle(2);
           }}
@@ -125,7 +126,7 @@ const BibliographicCards = () => {
         <ArticleOption
           className={activeArticle.id === 3 ? "active-card" : 0}
           type="image"
-          src="https://images.unsplash.com/photo-1506045412240-22980140a405?w=125&h=125&fit=crop"
+          src="https://images-na.ssl-images-amazon.com/images/I/91yu2bAqlDL.jpg"
           onClick={() => {
             setArticle(3);
           }}
@@ -133,7 +134,7 @@ const BibliographicCards = () => {
         <ArticleOption
           className={activeArticle.id === 4 ? "active-card" : 0}
           type="image"
-          src="https://images.unsplash.com/photo-1514041181368-bca62cceffcd?w=125&h=125&fit=crop"
+          src="https://images-na.ssl-images-amazon.com/images/I/81OthjkJBuL.jpg"
           onClick={() => {
             setArticle(4);
           }}
@@ -141,7 +142,7 @@ const BibliographicCards = () => {
         <ArticleOption
           className={activeArticle.id === 5 ? "active-card" : 0}
           type="image"
-          src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=123&h=125&fit=crop"
+          src="https://images-na.ssl-images-amazon.com/images/I/71Gr11n+1FL.jpg"
           onClick={() => {
             setArticle(5);
           }}
